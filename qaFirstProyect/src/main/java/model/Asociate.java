a9dbcf543bf59f69cb6b2aa9a4f26d25a2226a55
Merci13 <mrjorxe@gmail.com>
2020-03-06 02:02:25 -0600

    Update Asociate.java
    update

--- qaFirstProyect/src/main/java/model/Asociate.java
+++ qaFirstProyect/src/main/java/model/Asociate.java
@@ -11,4 +11,73 @@ package model;
  */
 public class Asociate {
     
+    String name;
+    int line;
+    String last_name;
+    Boolean status;
+    int id;
+
+    public Asociate(String name, int line, String last_name, Boolean status, int id) {
+        this.name = name;
+        this.line = line;
+        this.last_name = last_name;
+        this.status = status;
+        this.id = id;
+    }
+
+    
+    
+    public Asociate() {
+    }
+
+    
+    
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getLine() {
+        return line;
+    }
+
+    public void setLine(int line) {
+        this.line = line;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public Boolean getStatus() {
+        return status;
+    }
+
+    public void setStatus(Boolean status) {
+        this.status = status;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Asociate{" + "name=" + name + ", line=" + line + ", last_name=" + last_name + ", status=" + status + ", id=" + id + '}';
+    }
+    
+    
+    
+    
 }
